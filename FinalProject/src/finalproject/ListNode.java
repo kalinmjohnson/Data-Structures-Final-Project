b35a4066f5de00479709974300bc89bcc7ffb67a
KalinJohnson <kaj29995@bethel.edu>
2022-05-04 15:30:36 -0500

    Change from T to String type

--- FinalProject/src/finalproject/ListNode.java
+++ FinalProject/src/finalproject/ListNode.java
@@ -1,11 +1,11 @@
 package finalproject;
 
-public class ListNode<T> {
-	T data;
+public abstract class ListNode {
+	String data;
 	ListNode nxt;
 	ListNode prev;
 	
-	public ListNode(T data, ListNode<T> nxt, ListNode<T> prev) {
+	public ListNode(String data, ListNode nxt, ListNode prev) {
 		this.data = data;
 		this.nxt = nxt;
 		this.prev = prev;
---
+++ FinalProject/src/finalproject/Messages.java
@@ -0,0 +1,15 @@
+package finalproject;
+
+public class Messages extends ListNode<String> {
+
+    protected ListNode head;
+    protected int size;
+
+
+
+    public Messages () {
+        head = new ListNode<String><null, null, null>;
+        size = 0;
+    }
+
+}
