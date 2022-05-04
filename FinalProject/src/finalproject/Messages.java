63083051f490e7367d953eb17a5afbaba9b972a8
KalinJohnson <kaj29995@bethel.edu>
2022-05-04 15:47:51 -0500

    Add add and delete methods to messages

--- FinalProject/src/finalproject/Messages.java
+++ FinalProject/src/finalproject/Messages.java
@@ -5,7 +5,25 @@ public class Messages extends ListNode {
     protected ListNode head;
     protected int size;
 
+    public void addItem(ListNode newNode) {
+        newNode.prev = this;
+        newNode.nxt = this.nxt;
+        this.nxt.prev = newNode;
+        this.nxt = newNode;
+        size++;
+    }
 
+    public void deleteItem() {
+        this.prev.nxt = this.nxt;
+        this.nxt.prev = this.prev;
+    }
+
+    public Messages (String data) {
+        head = new ListNode(data, null, null);
+        head.nxt = head;
+        head.prev = head;
+        size = 1;
+    }
 
     public Messages () {
         head = new ListNode(null, null, null);
