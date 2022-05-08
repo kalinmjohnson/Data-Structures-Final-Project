69457a0f123925be02f3c8ca4975955958c2da13
JackFast1 <ngp62764@bethel.edu>
2022-05-08 15:41:11 -0500

    More files that need edit

---
+++ FinalProject/src/finalproject/BacklogController.java
@@ -0,0 +1,19 @@
+package finalproject;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+public class BacklogController implements EventHandler<ActionEvent> {
+	    public void addTask(String data) {
+	    	
+	    }
+
+	    public void deleteTask() {
+	    	
+	    }
+
+	    public void handle (ActionEvent e) {
+	    	
+	    }
+
+}
