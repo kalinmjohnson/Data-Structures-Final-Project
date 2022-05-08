40fd48a0df198c302593afe817c8c6e3e86630b7
JackFast1 <ngp62764@bethel.edu>
2022-05-08 15:46:21 -0500

    Files about the MinHeap thing

---
+++ FinalProject/src/finalproject/PriorityQueue.java
@@ -0,0 +1,45 @@
+package finalproject;
+
+import java.io.Serializable;
+import java.util.AbstractQueue;
+import java.util.Iterator;
+
+public class PriorityQueue<MinHeapNode> extends AbstractQueue<MinHeapNode> implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public boolean offer(MinHeapNode e) {
+		// TODO Auto-generated method stub
+		
+		return false;
+	}
+
+	@Override
+	public MinHeapNode poll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MinHeapNode peek() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<MinHeapNode> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
