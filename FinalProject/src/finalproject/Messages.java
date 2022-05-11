98813a7c6946d844f9c32af6da9f1f3dc6789652
Kalin Johnson <kalin.m.johnson@gmail.com>
2022-05-11 10:19:14 -0500

    Quick update

--- FinalProject/src/finalproject/Messages.java
+++ FinalProject/src/finalproject/Messages.java
@@ -5,17 +5,20 @@ public class Messages extends ListNode {
     protected ListNode head;
     protected int size;
 
-    public void addItem(ListNode newNode) {
-        newNode.prev = this;
-        newNode.nxt = this.nxt;
-        this.nxt.prev = newNode;
-        this.nxt = newNode;
+    public void addItem(ListNode newNode, ListNode currentNode) {
+        newNode.prev = currentNode;
+        newNode.nxt = currentNode.nxt;
+        currentNode.nxt.prev = newNode;
+        currentNode.nxt = newNode;
         size++;
     }
 
-    public void deleteItem() {
-        this.prev.nxt = this.nxt;
-        this.nxt.prev = this.prev;
+    public void deleteItem(ListNode currentNode) {
+    	if (currentNode == head) {
+    		head = currentNode.nxt;
+    	}
+    	currentNode.prev.nxt = currentNode.nxt;
+    	currentNode.nxt.prev = currentNode.prev;
     }
 
     public Messages (String data) {
