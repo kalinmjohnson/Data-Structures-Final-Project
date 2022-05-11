71701e1020fee4312bb8c3a65e2e395d988c5cfc
JackFast1 <ngp62764@bethel.edu>
2022-05-11 16:59:38 -0500

    Add files via upload

--- FinalProject/src/finalproject/addPriorityTaskPanel.java
+++ FinalProject/src/finalproject/addPriorityTaskPanel.java
@@ -1,5 +1,7 @@
 package finalproject;
 
+import java.util.PriorityQueue;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
@@ -14,17 +16,19 @@ import javafx.scene.layout.GridPane;
 public class addPriorityTaskPanel {
 	private TextField newTaskTF;
 	private TextField inputPriority;
-	private Label topTaskTF;
+	private Label topTaskL;
 	private Label topPriorityTF;
 	private Button addPTaskB;
 	private Button deleteTaskB;
 	
 	private GUI Gui;
+	// Create a new Min Heap of the priority Tasks (// model thingy)
+	private PriorityQueue<PriorityTask> priorityTasks = new PriorityQueue<PriorityTask>( 100);
 	
 	public addPriorityTaskPanel (GUI gui, BorderPane root) {
-		this.Gui = gui;
+		this.Gui = gui;	
 		
-		topTaskTF = new Label("Top Priority: " + "\n\nPriority Number:" + "\n");
+		topTaskL = new Label("Top Priority: " + "\n\nPriority Number:" + "\n");
 		
 		newTaskTF = new TextField();
 		inputPriority = new TextField();
@@ -34,43 +38,49 @@ public class addPriorityTaskPanel {
 		addPTaskB.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				
+				addTask( newTaskTF.getText(), Integer.valueOf( inputPriority.getText()));
 			}
 		});
 		deleteTaskB = new Button("-");
 		deleteTaskB.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			public void handle(ActionEvent event) {
-				
+				deleteTask();
 			}
 		});
 		
 		BorderPane display = new BorderPane();
 		
 		root.setTop(display);
-		display.setCenter(topTaskTF);
+		display.setCenter(topTaskL);
 		display.setRight(deleteTaskB);
 		root.setLeft(newTaskTF);
 		root.setCenter(inputPriority);
 		root.setRight(addPTaskB);
 		
 		BorderPane.setAlignment(deleteTaskB, Pos.TOP_RIGHT);
-		BorderPane.setAlignment(topTaskTF, Pos.TOP_LEFT);
+		BorderPane.setAlignment(topTaskL, Pos.TOP_LEFT);
 		
-		topTaskTF.setStyle("-fx-font: 16 arial;");
+		topTaskL.setStyle("-fx-font: 16 arial;");
 		
 		root.setPadding(new Insets(10, 10, 10, 10));
 		display.setPadding(new Insets(10, 0, 10, 0));
 		BorderPane.setMargin(inputPriority, new Insets(0, 30, 0, 30));
-		
-        
-        public void addTask(String data) {
-    	
-         }
+	}	
+   
+	public void addTask(String data, int pri) {
+		PriorityTask newTask = new PriorityTask( data, pri);
+		priorityTasks.add(newTask);
+		displayTask();
+	}
 
-          public void deleteTask() {
-    	
-          }
-		
-       	}
+    public void deleteTask() {
+		priorityTasks.poll(); // deletes the top task we could keep track of how many tasks were done
+		displayTask();
+    }
+	
+    public void displayTask() {
+    	topTaskL.setText( "Top Priority: " + priorityTasks.peek().rdata + 
+				"\n\nPriority Number:" + priorityTasks.peek().priority + "\n");
+    }
 }
