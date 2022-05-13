6e577eb00bac86422a621127f701486d9b12e374
Kalin Johnson <kalin.m.johnson@gmail.com>
2022-05-13 17:28:46 -0500

    add Java docs

--- FinalProject/src/finalproject/ListNode.java
+++ FinalProject/src/finalproject/ListNode.java
@@ -1,15 +1,38 @@
 package finalproject;
 
+/**
+ * Message controller
+ * 
+ * @author Kalin Johnson
+ * @author Nicholas Pederson
+ * @version Spring 2022
+ * 
+ */
+
 public class ListNode {
 	String data;
 	ListNode nxt;
 	ListNode prev;
+	
+	/**
+	 * ListNode Contructor with data
+	 * @param data
+	 * @param nxt
+	 * @param prev
+	 */
 	
 	public ListNode(String data, ListNode nxt, ListNode prev) {
 		this.data = data;
 		this.nxt = nxt;
 		this.prev = prev;
 	}
+	
+	/**
+	 * ListNode Contructor without data
+	 * @param data
+	 * @param nxt
+	 * @param prev
+	 */
 
 	public ListNode() {
 		this.data = null;
