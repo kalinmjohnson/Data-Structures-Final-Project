aff07da5b39fcf99e4a27bf4b0924538208fa421
Kalin Johnson <kalin.m.johnson@gmail.com>
2022-05-13 17:38:47 -0500

    Update Direction.java

--- FinalProject/src/finalproject/Direction.java
+++ FinalProject/src/finalproject/Direction.java
@@ -1,5 +1,14 @@
 package finalproject;
 
+/**
+* Direction Enum
+* 
+* @author Kalin Johnson
+* @author Nicholas Pederson
+* @version Spring 2022
+* 
+*/
+
 public enum Direction {
     UP, DOWN, LEFT, RIGHT
 }
