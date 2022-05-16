5596ad7809781b9d7c128ab1db1878c9ae76cda3
JackFast1 <ngp62764@bethel.edu>
2022-05-16 13:49:42 -0500

    Java dox and file name change (removed D:)

--- FinalProject/src/finalproject/addPriorityTaskPanel.java
+++ FinalProject/src/finalproject/addPriorityTaskPanel.java
@@ -1,5 +1,13 @@
 package finalproject;
 
+/**
+ * Priority Task Controller (and view).
+ * 
+ * @author Nicholas
+ * @author Kalin
+ * @version 5/16/2022
+ */
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileWriter;
@@ -13,10 +21,8 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
 
 public class addPriorityTaskPanel {
 	private TextField newTaskTF;
@@ -30,6 +36,11 @@ public class addPriorityTaskPanel {
 	// Create a new Min Heap of the priority Tasks (// model thingy)
 	private PriorityQueue<PriorityTask> priorityTasks = new PriorityQueue<PriorityTask>( 100);
 	
+	/**
+	 * Constructor for the Priority Task Panel
+	 * @param gui
+	 * @param root
+	 */
 	public addPriorityTaskPanel (GUI gui, BorderPane root) {
 		this.Gui = gui;	
 		
@@ -74,7 +85,7 @@ public class addPriorityTaskPanel {
 		
 		try {
 			// Creating an object of the file for reading the data
-			File myPri = new File("D:FileHandlingNewFilef1PT.txt");
+			File myPri = new File("FileHandlingNewFilef1PT.txt");
 			Scanner myReader = new Scanner(myPri);
 			//key = backLogTasks.head.nxt;
 			while (myReader.hasNextLine()) {
@@ -92,20 +103,31 @@ public class addPriorityTaskPanel {
 		}
 	}
 
+	/**
+	 * Adds a Task to the priority Queue and display it.
+	 * @param data
+	 * @param pri
+	 * @throws IllegalArgumentException
+	 */
 	public void addTask(String data, int pri) throws IllegalArgumentException{
 		if ( pri < 0) { throw new IllegalArgumentException("Priority can't be negative."); }
-		//if ( pri < 48 || pri > 57) { throw new IllegalArgumentException("Priority can't be anything except an int."); } //What happening here?
 		PriorityTask newTask = new PriorityTask( data, pri);
 		priorityTasks.add(newTask);
 		clear();
 		displayTask();
 	}
 	
+	/**
+	 * Delete the top priority Task.
+	 */
 	public void deleteTask() {
 		priorityTasks.poll(); // deletes the top task we could keep track of how many tasks were done
 		displayTask();
 	}
 	
+	/**
+	 * Display the Priority Task.
+	 */
     public void displayTask() {
     	String newTaskText = priorityTasks.isEmpty()?
     		("Top Priority: " + "\n\nPriority Number:" + "\n"):
@@ -114,16 +136,21 @@ public class addPriorityTaskPanel {
     	topTaskL.setText( newTaskText);
     }
     
+    /**
+     * Clears the text fields.
+     */
     public void clear() {
     	newTaskTF.setText("");
     	inputPriority.setText("");
-    	
     }
     
+    /**
+     * Writes out the data to the file.
+     */
     public void writeP() {
 		try {
 			// Creating an object of a file
-			File myPT = new File("D:FileHandlingNewFilef1PT.txt");
+			File myPT = new File("FileHandlingNewFilef1PT.txt");
 			if (myPT.createNewFile()) {
 				System.out.println("File created: " + myPT.getName());
 			} else {
@@ -135,7 +162,7 @@ public class addPriorityTaskPanel {
 		}
 		try {
 
-			FileWriter myWriter = new FileWriter("D:FileHandlingNewFilef1PT.txt");
+			FileWriter myWriter = new FileWriter("FileHandlingNewFilef1PT.txt");
 			
 			while (priorityTasks.peek() != null) {
 				PriorityTask sender = priorityTasks.poll();
